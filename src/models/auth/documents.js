a478aff29e7dd697048659ebd85c66229cb6d3a1
Saurabh Chamola <saurabh@pearlorganisation.com>
2024-11-05 10:14:47 +0530

    minor fix document schema

--- src/models/auth/documents.js
+++ src/models/auth/documents.js
@@ -4,8 +4,7 @@ const documentsSchema = new mongoose.Schema(
   {
     documentsCollection: [{ title: { type: String }, path: { type: String } }],
     user: { type: mongoose.Types.ObjectId, ref: "auth", required: false },
-    name: { type: String, required: true },
-    // document: { type: String, required: true },
+    name: { type: String, unique: true, required: true },
   },
   { timestamps: true }
 );
